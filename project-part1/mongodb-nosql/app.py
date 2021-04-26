544bcc945d08f079c4bad529b1403680bba646b7
Kevin <kjzhou2@illinois.edu>
2021-04-26 15:46:03 -0500

    redis section done

--- project-part1/mongodb-nosql/app.py
+++ project-part1/mongodb-nosql/app.py
@@ -7,6 +7,50 @@ from pymongo import MongoClient
 mongo = client = MongoClient('localhost', 27017)
 db = mongo["state-server"]
 
+@app.route('/<key>', methods=['PUT', 'DELETE', 'GET'])
+def key(key):
+    keys = db.keys
+    if request.method == 'PUT':
+        
+        if (keys.find_one_and_update({"key":key}, {'$push':{"values": request.data.decode('utf-8')}}) == None):
+            keys.insert_one({"key": key, "values": [request.data.decode('utf-8')]})
+            print("new document: ", keys.find_one({"key": key}))
+        return "Success", 200
+    elif request.method == 'DELETE':
+        keys.delete_one({"key":key})
+        return "Success", 200
+    elif request.method == 'GET':
+        doc = keys.find_one({"key": key})
+        if(doc == None):
+            return "Not found!", 404
+        values = doc["values"]
+        value = values[-1]
+        result = {
+            "value": value,
+            "version": len(values)
+            }
+        return jsonify(result), 200
+
+@app.route('/<key>/<version>', methods=['GET'])
+def getVersion(key, version):
+    keys = db.keys
+    print("inside GET version")
+    
+    doc = keys.find_one({"key": key})
+    print(key, version)
+    if (doc != None):
+        values = doc["values"]
+        if (int(version) <= len(values)):
+            value = values[int(version) - 1]
+            result = {
+                "value": value,
+                "version": int(version)
+                }
+            return jsonify(result), 200
+        else:
+            return "Not found!", 404
+    else: 
+        return "Not found!", 404
 
 # TODO:
 # PUT /<key> – Adds a versioned object
--- project-part1/redis-kv/app.py
+++ project-part1/redis-kv/app.py
@@ -5,6 +5,40 @@ app = Flask(__name__)
 import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+
+@app.route('/<key>', methods=['PUT', 'DELETE', 'GET'])
+def key(key):
+    if request.method == 'PUT':
+        #so each key will get its own list, then we can grab from that list
+        r.rpush(key, request.data.decode('utf-8'))
+        print(r.lindex(key, -1))
+        return "Success", 200
+    elif request.method == 'DELETE':
+        r.delete(key)
+        return "Success", 200
+    elif request.method == 'GET':
+        #key is a strings
+        if (r.exists(key)):  
+            print("GET", r.lindex(key, -1).decode())
+            result = {
+                "value": r.lindex(key, -1).decode(),
+                "version": r.llen(key)
+                }
+            print(result)
+            return jsonify(result), 200
+        else:
+            return "Not found!", 404
+
+@app.route('/<key>/<version>', methods=['GET'])
+def getVersion(key, version):
+    if (r.exists(key)): 
+        if (int(version) <= r.llen(key)): 
+            result = {
+                "value": r.lindex(key, int(version) - 1).decode(),
+                "version": int(version)
+                }
+            return jsonify(result), 200
+    return "Not found!", 404
 # TODO:
 # PUT /<key> – Adds a versioned object
 # GET /<key> – Retrieves the latest version of a key
