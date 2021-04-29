4b793a7069d676b1e3286b43d4bbf4959272399f
Kevin <kjzhou2@illinois.edu>
2021-04-29 13:06:02 -0500

    midterm finished

--- midterm2-microservice/app.py
+++ midterm2-microservice/app.py
@@ -1,20 +1,38 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-
+rooms = [300, 301, 302, 303, 304]
+guests = ["", "", "", "", ""]
 # PUT /guest/<guest>
 @app.route('/guest/<guest>', methods=["PUT"])
 def put_guest(guest):
-  return "", 500
+  for i in range(len(rooms)):
+    if not guests[i]:
+      guests[i] = guest
+      response = {"room": rooms[i],
+                  "guest": guest}
+      return jsonify(response), 200
+  return "No Rooms Available", 400
 
 
 # DELETE /guest/<guest>
 @app.route('/guest/<guest>', methods=["DELETE"])
 def delete_guest(guest):
-  return "", 500
+  for i in range(len(rooms)):
+    if (guests[i] == guest):
+      guests[i] = ""
+      response = {"room": rooms[i],
+                  "guest": guest}
+      return jsonify(response), 200
+  return "Guest Not Found", 404
 
 
 # GET /guest/<guest>
 @app.route('/guest/<guest>', methods=["GET"])
 def get_guest(guest):
-  return "", 500
+  for i in range(len(rooms)):
+    if (guests[i] == guest):
+      response = {"room": rooms[i],
+                  "guest": guest}
+      return jsonify(response), 200
+  return "Guest Not Found", 404
