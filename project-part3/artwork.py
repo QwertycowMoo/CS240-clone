26c6e60749f58d7f3eebf5240d28393f497d9c78
Kevin <kjzhou2@illinois.edu>
2021-05-07 21:11:29 -0500

    adding return and error handling for DreamAPI

--- project-part3/artwork.py
+++ project-part3/artwork.py
@@ -21,7 +21,9 @@ def post_image(key):
     },
     headers={'api-key': 'cf1a5000-d0a9-41b9-8929-ae97bd01aa52'}
   )
-
+  if r.status_code != 200 :
+    return "There's something wrong with the DeepDream API.", 500
+    
   ret = r.json()
   print(ret)
   print(ret['output_url'])
@@ -29,6 +31,6 @@ def post_image(key):
   file = open("temp_dream.jpg", "wb")
   file.write(img_r.content)
   file.close
-
+  img_dream = img_r.content
  
-  return '', 500
+  return img_dream, 200
